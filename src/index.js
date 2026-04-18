2fcd9849afe7ed435809af758869b3690138619f
ManishMandrai <manishkumarmandrai@gmail.com>
2026-04-18 14:01:55 +0530

    feat: add unpipe and vary modules with documentation and tests
    - Added unpipe module to unpipe streams from all destinations.
    - Included README, LICENSE, and HISTORY files for unpipe.
    - Implemented vary module to manipulate the HTTP Vary header.
    - Added README, LICENSE, and HISTORY files for vary.
    - Introduced wrappy module for callback wrapping utility.
    - Added README and LICENSE files for wrappy.
    - Created initial index.js for backend with dotenv configuration.
    - Added .gitkeep to public/images directory.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+const express = require('express')
+
+dotenv.config({
+    path: "./.env"
+});
+
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
